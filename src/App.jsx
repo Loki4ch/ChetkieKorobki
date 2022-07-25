9a5ba8d1bfeb4f2f59c339dd01c0f1c16a934465
Egor Philippov <goshaxp0@gmail.com>
2022-07-25 21:18:39 +0300

    render implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,26 @@
 import React from 'react';
-
+import productsArrState from './stateArray'
 class App extends React.Component {
     constructor(props) {
         super(props);
 
         this.state = {
             header: "Button 1",
-            productDivs: [
-                {
-                    id: 1,
-                    name: 'Product 1',
-                    price: '10$',
-                    amount: 1,
-                },
-                {
-                    id: 2,
-                    name: 'Product 2',
-                    price: '20$',
-                    amount: 2,
-                },
-                {
-                    id: 3,
-                    name: 'Product 3',
-                    price: '30$',
-                    amount: 3,
-                }
-            ],
+            productDivs: productsArrState,
         }
     }
-
 
     render() {
         return (
             <div className={'products'}>
-                <button className={'btn-edit'} onClick={(event) => {this.setState({})}}>{this.state.header}</button>
+                <button className={'btn-edit'} onClick={(event) => {this.setState({header: this.state.header + 1})}}>{this.state.header}</button>
                 {this.state.productDivs.map((elem, index) => {
                     return (
                         <div className={'product'}>
                             <p>{'ID: '}{elem.id}</p>
-                            <h1>{elem.name}</h1>
-                            <p>{elem.price}</p>
-                            <p>{elem.amount}</p>
+                            <h1>{'Name: '}{elem.name}</h1>
+                            <p>{'Price: '}{elem.price}</p>
+                            <p>{'Amount: '}{elem.amount}</p>
                         </div>
                     )
                 })}
---
+++ src/stateArray.js
@@ -0,0 +1,29 @@
+const productsArrState = [
+    {
+        id: 1,
+        name: 'Product 1',
+        price: '10$',
+        amount: 1,
+    },
+    {
+        id: 2,
+        name: 'Product 2',
+        price: '20$',
+        amount: 2,
+    },
+    {
+        id: 3,
+        name: 'Product 3',
+        price: '30$',
+        amount: 3,
+    }
+]
+
+// const ChangeId = () => {
+//       productsArrState.map(elem => {
+//           console.log(elem)
+//         elem.id = 99;
+//       })
+// }
+// export { ChangeId };
+export  default productsArrState;
