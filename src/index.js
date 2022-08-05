05960591fd2a6b2aeabf50d1b94df1ad92fea2ba
Egor Philippov <goshaxp0@gmail.com>
2022-08-05 22:45:50 +0300

    Auth done

--- src/index.js
+++ src/index.js
@@ -4,8 +4,10 @@ import App from './App.jsx';
 import "../css/reset.css";
 import "../css/style.css";
 import {GoogleOAuthProvider} from '@react-oauth/google';
+
 const root = ReactDom.createRoot(document.getElementById('root'));
 const clientId = '708566449515-17hiv1du9nklaptrbmh80r4on4ichata.apps.googleusercontent.com';
+
 root.render(
     <GoogleOAuthProvider clientId={clientId}>
         <App/>
